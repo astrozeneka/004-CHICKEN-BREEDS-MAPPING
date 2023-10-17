bf5af265d3a60914a91d57e148255fc875e8839e
astrozeneka <rasoarahonarivoniaina.h@ku.th>
2023-10-17 17:07:30 +0700

    update 2 test_map

--- 2_test_map.sh
+++ 2_test_map.sh
@@ -7,8 +7,26 @@
 #SBATCH -A proj5034
 
 FQ_DIR=/tarafs/scratch/proj5034-AGBKU/genome_NCBI/trimmed
-
 SEQ_LIST=(
   "LTR"
   "LTR+Int+LTR"
 )
+genomes=()
+while IFS= read -r genome; do
+    genomes+=("$genome")
+done < genomes.txt
+
+module purge
+module load SAMtools/1.9-intel-2019b
+
+for seq in "${SEQ_LIST[@]}"
+do
+  echo "${seq}"
+  for genome in "${genomes[@]}"
+  do
+    echo "${genome}"
+    echo bwa mem -t 96 \
+      "${FQ_DIR}/${genome}_trimmed.fq" \
+      ">" "data/map/${genome}.sam"
+  done
+done
