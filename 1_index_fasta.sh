728b891e88ca5a7c78893e596a0ab14cda4a8940
Ryan R <rasoarahonarivoniaina.h@ku.th>
2023-10-18 09:39:12 +0700

    Update 1_index_fasta.sh

--- 1_index_fasta.sh
+++ 1_index_fasta.sh
@@ -9,8 +9,15 @@ module purge
 module load BWA/0.7.17-intel-2019b
 
 SEQ_LIST=(
-  "LTR"
-  "LTR+Int+LTR"
+#  "LTR"
+#  "LTR+Int+LTR"
+  "ENS-1-x2"
+  "ENS-1-x3"
+  "ENS-1-x4"
+  "FlankL+LTR1"
+  "FlankL"
+  "FlankR"
+  "LTR+FlankR"
 )
 cd sequences
 for seq in "${SEQ_LIST[@]}"
